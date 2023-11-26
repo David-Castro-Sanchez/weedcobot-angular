e81ff3060cff61b2931e73b374a22b715779326b
Pau Llobet <pllobet@mychefpro.com>
2023-11-26 04:23:46 +0100

    Feat

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -16,38 +16,38 @@ export class HomeComponent {
   changesForm: FormGroup;
 
   constructor(private fb: FormBuilder, private afAuth: AngularFireAuth, private router: Router, private storageService: StorageService,
-    private apiService:ApiService) {
-      this.changesForm = this.fb.group({
-        maximum_temperature: ['', [Validators.required]],
-        minimum_temperature: ['', Validators.required],
-        maximum_air_humidity: ['', [Validators.required]],
-        minimum_air_humidity: ['', Validators.required],
-        maximum_soil_humidity: ['', [Validators.required]],
-        minimum_soil_humidity: ['', Validators.required]
-      })
-      apiService.getUser(this.storageService.SessionGetStorage("uid")).then(response => {
-        
-        this.changesForm.controls['maximum_temperature'].setValue(response.max_temperature);
-        this.changesForm.controls['minimum_temperature'].setValue(response.min_temperature);
-        this.changesForm.controls['maximum_air_humidity'].setValue(response.max_air_humidity);
-        this.changesForm.controls['minimum_air_humidity'].setValue(response.min_air_humidity);
-        this.changesForm.controls['maximum_soil_humidity'].setValue(response.max_soil_humidity);
-        this.changesForm.controls['minimum_soil_humidity'].setValue(response.min_soil_humidity);
+    private apiService: ApiService) {
+    this.changesForm = this.fb.group({
+      maximum_temperature: ['', [Validators.required]],
+      minimum_temperature: ['', Validators.required],
+      maximum_air_humidity: ['', [Validators.required]],
+      minimum_air_humidity: ['', Validators.required],
+      maximum_soil_humidity: ['', [Validators.required]],
+      minimum_soil_humidity: ['', Validators.required]
+    })
+    apiService.getUser(this.storageService.SessionGetStorage("uid")).then(response => {
+      let userSpan = document.getElementById("userId") as HTMLSpanElement;
+      userSpan.innerHTML = "User id: <br>" + this.storageService.SessionGetStorage("uid");
+      this.changesForm.controls['maximum_temperature'].setValue(response.max_temperature);
+      this.changesForm.controls['minimum_temperature'].setValue(response.min_temperature);
+      this.changesForm.controls['maximum_air_humidity'].setValue(response.max_air_humidity);
+      this.changesForm.controls['minimum_air_humidity'].setValue(response.min_air_humidity);
+      this.changesForm.controls['maximum_soil_humidity'].setValue(response.max_soil_humidity);
+      this.changesForm.controls['minimum_soil_humidity'].setValue(response.min_soil_humidity);
 
-        storageService.SessionAddStorage("user",response);
-      })
-
+      storageService.SessionAddStorage("user", response);
+    })
   }
 
   sendChanges() {
-   let user:UserModel = this.storageService.SessionGetStorage("user");
-   user.max_air_humidity = Number(this.changesForm.value.maximum_air_humidity);
-   user.min_air_humidity = Number(this.changesForm.value.minimum_air_humidity);
-   user.max_temperature = Number(this.changesForm.value.maximum_temperature);
-   user.min_temperature = Number(this.changesForm.value.minimum_temperature);
-   user.max_soil_humidity = Number(this.changesForm.value.maximum_soil_humidity);
-   user.min_soil_humidity = Number(this.changesForm.value.minimum_soil_humidity);
-   this.apiService.sendUser(user);
+    let user: UserModel = this.storageService.SessionGetStorage("user");
+    user.max_air_humidity = Number(this.changesForm.value.maximum_air_humidity);
+    user.min_air_humidity = Number(this.changesForm.value.minimum_air_humidity);
+    user.max_temperature = Number(this.changesForm.value.maximum_temperature);
+    user.min_temperature = Number(this.changesForm.value.minimum_temperature);
+    user.max_soil_humidity = Number(this.changesForm.value.maximum_soil_humidity);
+    user.min_soil_humidity = Number(this.changesForm.value.minimum_soil_humidity);
+    this.apiService.sendUser(user);
   }
 
   logout() {
@@ -59,5 +59,4 @@ export class HomeComponent {
 
     })
   }
-
 }
